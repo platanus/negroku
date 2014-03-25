6a9112de1e7ea7c9d5e22bf65c3b65795e10ae68
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-03-25 09:25:12 -0300

    Set rbenv defaults on load:defaults task

--- lib/negroku/deploy/rbenv.rb
+++ lib/negroku/deploy/rbenv.rb
@@ -2,6 +2,12 @@
 #
 # Adds capistrano/rbenv specific variables and tasks
 
-# Set the ruby version using the .ruby-version file
-# Looks for the file in the project root
-set :rbenv_ruby, File.read('.ruby-version').strip
+namespace :load do
+  task :defaults do
+
+    # Set the ruby version using the .ruby-version file
+    # Looks for the file in the project root
+    set :rbenv_ruby, File.read('.ruby-version').strip
+
+  end
+end
