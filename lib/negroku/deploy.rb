744917960550604a75de3b7cfc53d421efbe1d08
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-03-25 09:35:22 -0300

    Cosmetic change

--- lib/negroku/deploy.rb
+++ lib/negroku/deploy.rb
@@ -8,8 +8,8 @@ set :pty, true
 set :keep_releases, 5 unless respond_to?(:keep_releases)
 
 require 'negroku/helpers'
-load_deploy "rbenv" if was_required?('capistrano/rbenv')
-load_deploy "nodenv" if was_required?('capistrano/nodenv')
-load_deploy "bundler" if was_required?('capistrano/bundler')
-load_deploy "bower" if was_required?('capistrano/bower')
-load_deploy "unicorn" if was_required?('capistrano3/unicorn')
+load_deploy "rbenv"     if was_required? 'capistrano/rbenv'
+load_deploy "nodenv"    if was_required? 'capistrano/nodenv'
+load_deploy "bower"     if was_required? 'capistrano/bower'
+load_deploy "bundler"   if was_required? 'capistrano/bundler'
+load_deploy "unicorn"   if was_required? 'capistrano3/unicorn'
