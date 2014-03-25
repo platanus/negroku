dbf44052fb607b416f38d2155a9cce2e4ff6ae0b
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-03-25 09:22:00 -0300

    Set nodenv defaults on load:defaults task

--- lib/negroku/deploy/nodenv.rb
+++ lib/negroku/deploy/nodenv.rb
@@ -2,6 +2,12 @@
 #
 # Adds capistrano/nodenv specific variables and tasks
 
-# Set the node version using the .node-version file
-# Looks for the file in the project root
-set :nodenv_node, File.read('.node-version').strip
+namespace :load do
+  task :defaults do
+
+    # Set the node version using the .node-version file
+    # Looks for the file in the project root
+    set :nodenv_node, File.read('.node-version').strip
+
+  end
+end
