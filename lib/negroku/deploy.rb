fd7fd11edef68ad973af8ec11aa64ac184f31271
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-03-25 11:14:46 -0300

    Remove negroku:deploy:setup task

--- lib/negroku/deploy.rb
+++ lib/negroku/deploy.rb
@@ -7,23 +7,6 @@ set :pty, true
 
 set :keep_releases, 5
 
-# Ensure the folders needed exist
-namespace :negroku do
-  namespace :deploy do
-    desc "Setup the application system requirements"
-    task :setup do
-      if was_required? 'capistrano/nginx'
-        invoke 'nginx:site:add'
-        invoke 'nginx:site:enable'
-        invoke 'nginx:reload'
-      end
-
-      invoke 'negroku:unicorn:restart' if was_required? 'capistrano3/unicorn'
-
-    end
-  end
-end
-
 require 'negroku/helpers'
 load_deploy "rbenv"     if was_required? 'capistrano/rbenv'
 load_deploy "nodenv"    if was_required? 'capistrano/nodenv'
