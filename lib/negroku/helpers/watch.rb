9e1d8e5b5783138eb64ca74f479f4395258c700d
Juan Ignacio Donoso <jidonoso@gmail.com>
2015-03-05 16:46:00 -0300

    fix(eye): add build in template and name options to dls watch

--- lib/negroku/helpers/watch.rb
+++ lib/negroku/helpers/watch.rb
@@ -1,7 +1,13 @@
 def watch_process(name, options = {})
   processes = fetch(:eye_watched_processes, {})
 
-  options[:template] ||= "tasks/eye/_#{name}.erb"
+  options[:name] ||= name
+
+  options[:template] ||= name.to_sym
+
+  if options[:template].kind_of?(Symbol)
+    options[:template] = "tasks/eye/_#{options[:template]}.erb"
+  end
 
   processes[name] = options
 
