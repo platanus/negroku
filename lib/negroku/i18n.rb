41b084c6bbf95900cb25f1a5269377e1a026bae2
Ignacio Baixas <iobaixas@gmail.com>
2015-07-08 11:15:01 -0300

    fix(i18n): fixes negroku overriding i18n load paths

--- lib/negroku/i18n.rb
+++ lib/negroku/i18n.rb
@@ -2,7 +2,7 @@ require 'i18n'
 require 'set'
 
 locale_path = File.join(File.dirname(__FILE__), 'locales')
-I18n.load_path = Dir[File.join(locale_path, '*.yml')]
+I18n.load_path.concat Dir[File.join(locale_path, '*.yml')]
 I18n.backend.load_translations
 
 if I18n.respond_to?(:enforce_available_locales=)
