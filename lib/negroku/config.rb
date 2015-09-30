55552f32034cc913ace2de434965ae314195a6e3
Juan Ignacio Donoso <jidonoso@gmail.com>
2015-09-29 20:55:03 -0400

    style(): fix some styles and change a deprecated method

--- lib/negroku/config.rb
+++ lib/negroku/config.rb
@@ -18,11 +18,11 @@ class Negroku::ConfigFactory
   end
 
   attribute :bower, Negroku::Feature, default: {
-    name: "bower", enabled: File.exists?('bower.json')
+    name: 'bower', enabled: File.exist?('bower.json')
   }
 
   attribute :bundler, Negroku::Feature, default: {
-    name: "bundler", enabled: File.exists?('Gemfile')
+    name: 'bundler', enabled: File.exist?('Gemfile')
   }
 
   attribute :delayed_job, Negroku::Feature, default: {
@@ -31,39 +31,38 @@ class Negroku::ConfigFactory
   }
 
   attribute :nginx, Negroku::Feature, default: {
-    name: "nginx", enabled: true, required: true
+    name: 'nginx', enabled: true, required: true
   }
 
   attribute :nodenv, Negroku::Feature, default: {
-    name: "nodenv", enabled: true, required: true
+    name: 'nodenv', enabled: true, required: true
   }
 
   attribute :rails, Negroku::Feature, default: {
-    name: "rails", enabled: loaded_in_bundler('rails')
+    name: 'rails', enabled: loaded_in_bundler('rails')
   }
 
   attribute :rbenv, Negroku::Feature, default: {
-    name: "rbenv", enabled: true, required: true
+    name: 'rbenv', enabled: true, required: true
   }
 
   attribute :sphinx, Negroku::Feature, default: {
-    name: "sphinx", enabled: loaded_in_bundler('thinking-sphinx')
+    name: 'sphinx', enabled: loaded_in_bundler('thinking-sphinx')
   }
 
   attribute :unicorn, Negroku::Feature, default: {
-    name: "unicorn", enabled: loaded_in_bundler('unicorn')
+    name: 'unicorn', enabled: loaded_in_bundler('unicorn')
   }
 
   attribute :puma, Negroku::Feature, default: {
-    name: "puma", enabled: loaded_in_bundler('puma')
+    name: 'puma', enabled: loaded_in_bundler('puma')
   }
 
   attribute :whenever, Negroku::Feature, default: {
-    name: "whenever", enabled: File.exists?('config/schedule.rb')
+    name: 'whenever', enabled: File.exist?('config/schedule.rb')
   }
 
   # private
-
 end
 
 Negroku::Config = Negroku::ConfigFactory.new
